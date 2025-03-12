5d322d099809b24b56f834a21eb209972cba9a2a
古田健 <kenfuruta0824@gmail.com>
2025-03-13 07:51:10 +0900

    onPlay 関数への単一の呼び出しに置き換え

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ function Board({ xIsNext, squares, onPlay }) {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
